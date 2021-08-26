a696ef701a4e6044f1707526d69e37b2b662e33b
atiwat_t <atiiwwat@gmail.com>
2021-08-26 14:55:02 +0700

    call Plugin TextRec

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HuaweiMlkit } from 'hwmlkit';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,41 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
+  result: any;
+  image: any;
+  strImage: any;
+
   constructor() {}
+
+  async onClickTextRec() {
+    await HuaweiMlkit.textRec({ value: this.strImage })
+      .then((res: any) => {
+        this.result = res.value;
+        console.log('result ==>', this.result.value);
+      })
+      .catch((err) => {
+        this.result = err;
+        console.log('err ==>', this.result);
+      });
+  }
+
+  onClickInput() {
+    document.getElementById('inputimage').click();
+  }
+
+  fileChangeChange(event) {
+    if (event.target.files && event.target.files[0]) {
+      const reader = new FileReader();
+      reader.onload = (event: any) => {
+        this.image = event.target.result;
+        this.strImage = this.image.replace(/^data:image\/\w+;base64,/, "");
+        console.log('imageAds', this.image);
+
+        console.log('strImage =>', this.strImage);
+        
+      };
+      reader.readAsDataURL(event.target.files[0]);
+    }
+  }
 
 }
