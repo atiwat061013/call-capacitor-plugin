0fa05414c30d6b1909f2f6049f4960a71011ad99
Aiwat Thongmak <atiiwwat@gmail.com>
2021-11-01 23:09:36 +0700

    call faceVerfication

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { HuaweiMlkit } from 'hwmlkit';
+import {DomSanitizer} from '@angular/platform-browser';
+
 
 @Component({
   selector: 'app-home',
@@ -13,8 +15,15 @@ export class HomePage {
 
   imageFaceDetec: any;
   resultFaceDetec: any;
+  resultFaceVerification: any;
 
-  constructor() {}
+  imageVerifiTem: any;
+  imageVerifiCom: any;
+
+  resultPicVerifiTem: any;
+  resultPicVerifiCom: any;
+
+  constructor(private sanitizer: DomSanitizer) {}
 
   async onClickTextRec() {
     await HuaweiMlkit.TextRecognition({language:"en", base64: this.image?.replace(/^data:image\/\w+;base64,/, "")})
@@ -73,6 +82,56 @@ export class HomePage {
       });
   }
 
+  onClickInputFaceVerifiTem(){
+    document.getElementById('inputFaceVerifiTem').click();
+  }
+
+  fileFaceVerifiTemChange(event) {
+    if (event.target.files && event.target.files[0]) {
+      const reader = new FileReader();
+      reader.onload = (event: any) => {
+        this.imageVerifiTem = event.target.result;
+        console.log('imageVerifiTem', this.imageVerifiTem);
+        
+      };
+      reader.readAsDataURL(event.target.files[0]);
+    }
+  }
+
+  ///com
+  onClickInputFaceVerifiCom(){
+    document.getElementById('inputFaceVerifiCom').click();
+  }
+
+  fileFaceVerifiComChange(event) {
+    if (event.target.files && event.target.files[0]) {
+      const reader = new FileReader();
+      reader.onload = (event: any) => {
+        this.imageVerifiCom = event.target.result;
+        console.log('imageVerifiCom', this.imageVerifiCom);
+        
+      };
+      reader.readAsDataURL(event.target.files[0]);
+    }
+  }
+
+
+  async onClickFaceVerification() {
+    await HuaweiMlkit.FaceVerification({faceTemplate: this.imageVerifiTem?.toString().replace(/^data:image\/\w+;base64,/, ""), faceCompare: this.imageVerifiCom?.toString().replace(/^data:image\/\w+;base64,/, "")}).then((res: any) => {
+      
+      this.resultFaceVerification = res;
+      console.log('FaceVerification ==>',this.resultFaceVerification);
+      this.resultPicVerifiTem = this.sanitizer.bypassSecurityTrustUrl("data:Image/*;base64,"+res.picTemplate);
+      this.resultPicVerifiCom = this.sanitizer.bypassSecurityTrustUrl("data:Image/*;base64,"+res.picCom);
+      
+      // console.log('this.imageVerifiTem ==>',this.imageVerifiTem);
+      
+      
+    }).catch((err) => {
+      this.resultFaceVerification = err;
+      console.log('err ==>', this.resultFaceVerification);
+    });
+  }
 
 
   // async onClickFaceVerification() {
